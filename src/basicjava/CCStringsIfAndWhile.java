6cfa43a9a60b39b2eb70cd622c95762a5f9e64cb
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-02-08 22:13:19 +0000

    added comments

--- src/basicjava/CCStringsIfAndWhile.java
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -3,7 +3,7 @@ package basicjava;
 public class CCStringsIfAndWhile {
 
 	public static boolean isDigit(char c ) {		
-
+		//simple checking with character transmitted 
 		if(c >= '0' && c <= '9' ){
 			return true;
 		}
@@ -13,10 +13,13 @@ public class CCStringsIfAndWhile {
 
 	public static int count(String test, String letters) {
 		int testLength = test.length();
+		//important to change to lower case so it does not take that into account
 		letters = letters.toLowerCase();
 		test = test.toLowerCase();
 		int counter = 0;
 		
+		//two while loops one for the length of the test and the other for the letters
+
 		while(testLength >= 1){
 			testLength--;
 			char currentTestLetter = test.charAt(testLength);
@@ -42,10 +45,11 @@ public class CCStringsIfAndWhile {
 	public static int smallestDigit(int i) {
 		int num = 9;
 		int smallest = 9;
+		//for a special case that you do not need to move forward as zero will always be lowest number
 		if (i == 0) {
 			return 0;
 		}
-
+//reoccuring until last digit to check one isnt smaller than the other based on hierchay
 		while (i != 0) {
 			num = i % 10;
 			
