3d25406a5d0a15e0cea4bc9ace9ccf19eaffa0db
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-02-25 23:09:19 +0000

    starting coding challenge arraylists

---
+++ src/basicjava/CCArrayList.java
@@ -0,0 +1,17 @@
+package basicjava;
+
+import java.util.ArrayList;
+
+public class CCArrayList {
+	
+	public static int indexOfIgnoreCase(ArrayList<String> strs,String toFind) {
+		return 0;
+	}
+
+//used wrapper class Double for ArrayList since a Double cannot be used as a type of argument for generics
+	public static void insert(ArrayList<Double>nums,double numToInsert, int insertAtIndex) {
+		
+
+	}
+	}
+
