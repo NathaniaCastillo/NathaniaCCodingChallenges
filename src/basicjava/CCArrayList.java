d223dcb6413e8c8bab78b86bf3e9f4f62b3862de
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-02-26 00:22:38 +0000

    first method done

--- src/basicjava/CCArrayList.java
+++ src/basicjava/CCArrayList.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class CCArrayList {
 	
 	public static int indexOfIgnoreCase(ArrayList<String> strs,String toFind) {
-		//Arrays.sort(strs,String.CASE_INSENSITIVE_ORDER);
+		
 		for(int num = 0; num < strs.size(); num++) {
-			String str = strs.get(num);
-			if(str.equalsIgnoreCase(toFind)){
+			String str = strs.get(num);//new .get() is used to find the certain string in array 
+			if(str.equalsIgnoreCase(toFind)){//new method here ignore if it is upper case or lower case for string, learnt in chat gpt
 				return num;
 			}
 				
@@ -23,7 +23,6 @@ public class CCArrayList {
 //used wrapper class Double for ArrayList since a Double cannot be used as a type of argument for generics
 	public static void insert(ArrayList<Double>nums,double numToInsert, int insertAtIndex) {
 		
-
 	}
 	}
 
