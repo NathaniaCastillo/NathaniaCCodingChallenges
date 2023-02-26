3f2c239796c1d16e999a0909f938661981620316
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-02-26 21:12:28 +0000

    created the instances,constructors and methods in new class.

---
+++ src/oosequence/Flight.java
@@ -0,0 +1,24 @@
+package oosequence;
+
+import java.util.Date;
+
+public class Flight {
+	Date departure;
+	Date arrival;
+	
+	public Flight(Date time1, Date time2) {
+		
+	}
+	
+	public Flight(Flight flightToCopy) {
+		
+	}
+
+	
+	public long length() {
+		return 0;
+		
+		
+	}
+
+}
