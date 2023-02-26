af1e73c49d892aea2dc0bdb39df0f016a90e6c29
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-02-26 00:22:25 +0000

    first method done

--- src/basicjava/CCArrayList.java
+++ src/basicjava/CCArrayList.java
@@ -1,12 +1,24 @@
 package basicjava;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CCArrayList {
 	
 	public static int indexOfIgnoreCase(ArrayList<String> strs,String toFind) {
-		return 0;
-	}
+		//Arrays.sort(strs,String.CASE_INSENSITIVE_ORDER);
+		for(int num = 0; num < strs.size(); num++) {
+			String str = strs.get(num);
+			if(str.equalsIgnoreCase(toFind)){
+				return num;
+			}
+				
+			
+		}
+		
+		
+		return -1;
+}
 
 //used wrapper class Double for ArrayList since a Double cannot be used as a type of argument for generics
 	public static void insert(ArrayList<Double>nums,double numToInsert, int insertAtIndex) {
