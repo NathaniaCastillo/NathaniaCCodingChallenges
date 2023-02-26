019e6c48772adb0e2e7ecda800f8ae3358a13fb0
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-02-26 21:35:12 +0000

    I think I got the constructor with two arguments right.

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -6,7 +6,21 @@ public class Flight {
 	Date departure;
 	Date arrival;
 	
-	public Flight(Date time1, Date time2) {
+	public Flight (Date time1, Date time2) {
+		if (time1 == (null)|| time2 == (null)) {
+			departure = time1;
+			arrival = time2;
+		}
+		else if (time1 != (null) && time2 != (null)) {
+			if (time1.before(time2)){
+				departure = time1;
+				arrival = time2;
+			}
+			else {
+				departure = (null);
+				arrival = (null);
+			}
+		}
 		
 	}
 	
