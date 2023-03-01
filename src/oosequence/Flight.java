7a03298219d1446a39968345eb0adce5803e90b5
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-03-01 22:56:21 +0000

    I changed line 37 with the help of matthew to realDeparture instead of just departure.

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -6,10 +6,44 @@ package oosequence;
 import java.util.Date;
 
 public class Flight {
-	Date departure;
-	Date arrival;
 	
-	public Flight (Date time1, Date time2) {
+	private Date departure;
+	private Date arrival;
+	
+	public Object getDeparture() {
+		return departure;
+	}
+
+	public Object getArrival() {
+		return arrival;
+	}
+
+	public void setArrival(Date realArrival) {//setting only arrival date
+		if (departure == (null) || realArrival == (null)) {
+			arrival = realArrival;//no more tests at this point
+		}
+		else if (departure.before(realArrival)){
+			arrival = realArrival;//this refers to instance and second is the local variable?
+			
+			// --> then departure date 
+		}
+			
+	}
+
+	public void setDeparture(Date realDeparture){//is this the one for when a departure changes? {//setting departure computer
+		if(arrival == (null)|| realDeparture == (null)) {
+			departure = realDeparture;;//idk
+		}
+		else if(realDeparture.before(arrival)) {//idkkkkk
+			departure = realDeparture;
+	}
+	
+		}
+
+	
+
+	
+	public Flight (Date time1, Date time2) {//this naming either
 		if (time1 == (null)|| time2 == (null)) {
 			departure = time1;
 			arrival = time2;
@@ -37,7 +71,7 @@ public class Flight {
 	public long length() {
 		long realLength = 0;
 		if (departure != (null)&& arrival != (null)) {
-			long minutesDeparture = departure.getTime();//used get time to switch from date to long found byusing chat gpt.
+			long minutesDeparture = departure.getTime();//used get time to switch from date to long found by using chat gpt.
 			long minutesArrival = arrival.getTime();
 
 			realLength = (minutesArrival - minutesDeparture)/60000;
@@ -48,24 +82,5 @@ public class Flight {
 		
 	}
 
-	public Object getDeparture() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Object getArrival() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void setArrival(Date date) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setDeparture(Date date) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
