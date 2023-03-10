3e8d99e20ecbadbb51096a6688014c0628fb0ab8
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-03-10 21:40:24 +0000

    no errors in FlightTest.java--> everything is running

---
+++ src/oosequence/Flight.java
@@ -0,0 +1,78 @@
+package oosequence;
+
+import java.util.Date;
+
+public class Flight {
+	private String departureAirport;
+	private String arrivalAirport;
+	
+	
+	
+	
+	
+	public void setArrivalAirport(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	public Object getArrivalAirport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+	public Object getDepartureAirport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+	public void setDepartureAirport(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+//maybe do inheritance here or something??
+	public void setStart(Date date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	public void setEnd(Date date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	public Object getDuration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	
+	
+
+}
+
+
