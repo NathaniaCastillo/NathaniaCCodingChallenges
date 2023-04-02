155c645998a48fb27ab3e40e56c3722350a92875
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-04-02 16:03:59 -0600

    have eadded the two methods may have done it in the wrong class

---
+++ src/basicjava/FileExercises.java
@@ -0,0 +1,15 @@
+package basicjava;
+
+public class FileExercises {
+
+	public static int counting(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void toUpper(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
