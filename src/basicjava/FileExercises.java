4d88f5355e1f7435058e192321725d51e1205ff9
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-04-03 21:45:27 -0600

    I have gotten the first method to work. Need to work on how to gte the right index as that is where I was going wrong

--- src/basicjava/FileExercises.java
+++ src/basicjava/FileExercises.java
@@ -1,14 +1,35 @@
 package basicjava;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
 public class FileExercises {
 
-	public static int counting(String string) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static int counting(String word) throws FileNotFoundException, IOException{
+		int counter = 0;
+		System.out.println("++++"+word);
+		BufferedReader reader = new BufferedReader(new FileReader("example.txt"));
+		String line;
+		while((line = reader.readLine()) != null) {
+			String[] words = line.split(" ");
+			for(int i = 0; i < words.length; i++) {
+				String wordIndex = words[i];
+				if(wordIndex.equalsIgnoreCase(word)) {
+					counter++;
+				}
+		}
+		
+		}
+		
+		
+		return counter;
 	}
 
-	public void toUpper(String string, String string2) {
-		// TODO Auto-generated method stub
+	public void toUpper(String iFName, String oFName) throws FileNotFoundException, IOException {
+		
 		
 	}
 
