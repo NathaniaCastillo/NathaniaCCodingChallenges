c8ad569de639a626744dcb4a9964c6b46d941454
Nathania Castillo <nathania.castillo@ucalgary.ca>
2023-04-03 22:37:02 -0600

    finished the second method, it is pretty much the same thing as first but just using writer.

--- src/basicjava/FileExercises.java
+++ src/basicjava/FileExercises.java
@@ -4,19 +4,20 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 
 public class FileExercises {
 
 	public static int counting(String word) throws FileNotFoundException, IOException{
 		int counter = 0;
-		System.out.println("++++"+word);
 		BufferedReader reader = new BufferedReader(new FileReader("example.txt"));
-		String line;
+		String line;  
 		while((line = reader.readLine()) != null) {
 			String[] words = line.split(" ");
 			for(int i = 0; i < words.length; i++) {
-				String wordIndex = words[i];
+				String wordIndex = words[i];//was doing this part wrong and chta gpt showed me how to do it in a different way
 				if(wordIndex.equalsIgnoreCase(word)) {
 					counter++;
 				}
@@ -28,9 +29,18 @@ public class FileExercises {
 		return counter;
 	}
 
-	public void toUpper(String iFName, String oFName) throws FileNotFoundException, IOException {
-		
+	public void toUpper(String iFName, String oFName) throws IOException {
+		try (
+		BufferedReader reader = new BufferedReader(new FileReader(iFName));//took ths try method from chat gpt
+		BufferedWriter writer = new BufferedWriter(new FileWriter(oFName));
+		){
+		String line;
+		while((line = reader.readLine())!= null) {
+			writer.write(line.toUpperCase());
+			writer.newLine();
+		}
 		
 	}
 
+}
 }
